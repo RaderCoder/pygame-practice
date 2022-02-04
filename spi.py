42d329dc0b000877bb877a07d503dded3952074c
Radee Al Mahmood <radee.mahmood@northsouth.edu>
2022-02-04 09:59:39 +0600

    Assets added

--- spi.py
+++ spi.py
@@ -2,3 +2,14 @@ import pygame
 import os
 import time
 import random
+
+RED_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_blue_small.png"))
+
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_yellow.png"))
+
+RED_LASER = pygame.image.join(os.path.join("assets","pixel_laser_red.png"))
+GREEN_LASER = pygame.image.join(os.path.join("assets","pixel_laser_green.png"))
+BLUE_LASER = pygame.image.join(os.path.join("assets","pixel_laser_blue.png"))
+YELLOW_LASER = pygame.image.join(os.path.join("assets","pixel_laser_yellow.png"))
