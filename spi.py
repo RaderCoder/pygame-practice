850998bc0018797be9bff8ad333c256a1d6aa862
Radee Al Mahmood <radee.mahmood@northsouth.edu>
2022-02-04 11:26:53 +0600

    added enemy classes

--- spi.py
+++ spi.py
@@ -52,12 +52,31 @@ class Player(Ship):
         self.mask = pygame.mask.from_surface(self.ship_img)
         self.max_health = health
 
+class Enemy(Ship):
+    COLOR_MAP = {
+                "red": (RED_SPACE_SHIP,RED_LASER),
+                "green":(GREEN_SPACE_SHIP,GREEN_LASER),
+                "blue": (BLUE_SPACE_SHIP,BLUE_LASER)
+    }
+
+    def __init__(self,x,y,color,health=100):
+        super().__init__(x,y,health)
+        self.ship_img,self.laser_img = self.COLOR_MAP[color]
+        self.mask = pygame.mask.from_surface(self.ship.img)
+
+    def move(self,vel):
+        self.y += vel
+
 def main():
     run = True
     FPS = 60
-    level=1
-    lives=5
+    level = 0
+    lives = 5
     main_font = pygame.font.SysFont("comicsans",50)
+
+    enemies = []
+    wave_length = 5
+    enemy_vel
 
     player_vel = 5
 
@@ -74,13 +93,15 @@ def main():
         WIN.blit(lives_label,(10,10))
         WIN.blit(level_label,(WIDTH - level_label.get_width()-10,10))
 
+        for enemy in enemies:
+            enemy.draw(WIN)
+
         player.draw(WIN)
 
         pygame.display.update()
 
     while run:
         clock.tick(FPS)
-        redraw_window()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -100,5 +121,6 @@ def main():
             #down
             player.y += player_vel
 
+        redraw_window()
 
 main()
